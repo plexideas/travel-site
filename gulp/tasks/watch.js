39125bdb2f232e13fe476b87b706fd730e0a5755
Serj Sakharovskiy <8bitago@Serjs-MacBook-Pro.local>
2019-03-11 00:45:22 +0300

    Completed large hero block and intro content block.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const browserSync = require('browser-sync').create();
+
+gulp.task('watch', () => {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', () => {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', () => {
+    gulp.start('cssInject');
+  });
+})
+
+gulp.task('cssInject', ['styles'], () => {
+  gulp.src('./app/temp/styles/style.css')
+    .pipe(browserSync.stream());
+});
